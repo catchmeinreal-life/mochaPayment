ae687b2bba63deac5b69e3280bc654f4c1fa9c3d
eric matutu <ericmatutu125@gmail.com>
2025-06-14 19:01:38 +0300

    payment-api set - adding the routes

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const express = require('express');
 //path
 const path = require('path');
 
+//cors
+const cors = express('cors');
+
 
 dotenv.config({ path: './config.env' });
 
 const app = express();
 app.use(express.json());
+
+app.use(cors);
 
 // serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,6 +24,33 @@ app.set('view engine', 'html');
 //routes
 const moviesRoute = require('./src/routes/moviesRoutes.js')
 app.use('/movies', moviesRoute);
+
+//payment api
+const demoUsers = [
+    { name: "Matutu", accountNumber: "123456789", balance: 500},
+    { name: "Bob", accountNumber: "987654321", balance: 800}
+]
+
+app.post('/pay', (req, res) => {
+    const { sender, receiver, amount } = req.body;   //account numbers
+
+    const senderAccount = demoUsers.find( (user) => user.accountNumber === sender);
+    const receiverAccount = demoUsers.find( (user) => user.accountNumber === receiver);
+
+    //handle case where accounts number is wrong
+    if (!senderAccount || ! receiverAccount) {
+        return res.status(400).json({error: "Invalid Accounts numbers"});
+    }
+
+    if (senderAccount.balance < amount ) {
+        return res.status(400).json({error: "insufficiet funds"});
+    }
+
+    senderAccount.balance -= amount;
+    receiverAccount.balance += amount;
+
+    res.status(200).json({message: "Transaction successful", senderAccount, receiverAccount})
+})
 
 
 // Render HTML file
