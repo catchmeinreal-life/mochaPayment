bda5a1028829cdeb4db644d2cc31e66dd1b54a43
eric matutu <ericmatutu125@gmail.com>
2025-06-14 18:30:12 +0300

    added bank-payment ui

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const paymentForm = () => {
+  const [ sender, setSender] = useState('');
+  const [ receiver, setReceiver] = useState('');
+  const [ amount, setAmount] = useState('');
+
+  const handlePayment = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({sender, receiver, amount: Number(amount)})
+      })
+
+      const data = await response.json();
+      alert(data.message || data.error);
+    } catch (error) {
+      console.error("payment error: ", error)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Bank Transfer Payment</h1>
+      <input type="text" name="" id="" placeholder='Sender Account' onChange={e => setSender(e.target.value)} />
+      <input type="text" name="" id="" placeholder='Receiver Account' onChange={e => setReceiver(e.target.value)} />
+      <input type="number" name="" id="" placeholder='Amount' onChange={e => setAmount(e.target.value)}/>
 
-export default App
+
+
+      <p>{sender}, {receiver}, {amount}</p>
+      <button onClick={handlePayment}>Send Payment</button>
+    </div>
+  );
+};
+
+export default paymentForm;
