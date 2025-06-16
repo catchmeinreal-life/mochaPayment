ae4070c18a859337c27e53962780d9e99731cb3a
eric matutu <ericmatutu125@gmail.com>
2025-06-17 00:53:28 +0300

    installed a dependency react-router-dom

---
+++ my-react-app/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+
+
+/**localhost:5175/ */
+
+function Home() {
+    return (
+        <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of your React application.</p>
+        <p>You can navigate to other pages using the links in the navigation bar.</p>
+        </div>
+    );
+}
+
+export default Home;
