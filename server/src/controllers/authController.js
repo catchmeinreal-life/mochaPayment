8ca63b0e4e08c9a19c86767d3647c44cc87b0671
eric matutu <ericmatutu125@gmail.com>
2025-07-13 04:37:11 +0300

    added console.log for debugging returning null for User.findOne({email})

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,10 +1,11 @@
-const User = require('../models/User.js');
+const User = require('../models/User.JS');
 
 module.exports = loginUser = async (req, res) => {
-
+    console.log(req.body);
     const {email, password} = req.body;
     //check for email
     const user = await User.findOne({email});
+    console.log(user);
     if (!user) {
         return res.status(204).json({message: "no user by that email"});
     }
