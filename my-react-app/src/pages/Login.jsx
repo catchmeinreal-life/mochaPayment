d30c29bc0e3920029ed191a2862720a0b1b72748
eric matutu <ericmatutu125@gmail.com>
2025-07-14 22:52:15 +0300

    imported the authservice: from MochaApi axios useffect to get the greeting message from the server then showing the message and showing it on the newly created form

--- my-react-app/src/pages/Login.jsx
+++ my-react-app/src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { useNavigate, Navigate } from 'react-router-dom';
+import { useNavigate, Navigate} from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../components/NavBar';
+
+import { authService } from '../services/mochaPayment';
+
 import '../styles/login.css'; //styling
 import '../styles/global.css';
-
 import '../styles/login.css';
 
 function Login({ isAuthenticated }) {
@@ -15,11 +18,26 @@ function Login({ isAuthenticated }) {
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState("");
 
+  useEffect(() => {
+      const greetUser = async () => {
+        try {
+          const response = await authService.signIn();
+          setMessage(response.data.message);
+        } catch (error) {
+          setError(error.message)
+        }
+      }
+      greetUser(); // on componet mount 
+    }, []);
+
+
+
   const handleLogin = async (e) => {
     e.preventDefault();
     setIsLoading(true);
     try {
       const response = await authService.login({ email, password });
+      setMessage(response.message);
       console.log("Login response:", response.message);
       // if (response.success) {
       //   localStorage.setItem("isAuthenticated", "true");
@@ -41,9 +59,23 @@ function Login({ isAuthenticated }) {
       <>
         <NavBar isAuthenticated={isAuthenticated} />
         <div className="login-page">
+          <h1>Login In Page</h1>
           <div className="message">
             <p>{ message ? message : error}</p>
           </div>
+          <p>don't have an Account?<span><Link to='/signin' replace>Sign-In</Link></span></p>
+          <form onSubmit={handleLogin}>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" required value={email} onChange={e => setEmail(e.target.value)} autoComplete='user@email.com'/>
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" required value={password} onChange={e => setPassword(e.target.value)} autoComplete='password' />
+            </div>
+            
+            <button type="submit">Sign-In</button>
+          </form>
        </div>
     </>
   )
