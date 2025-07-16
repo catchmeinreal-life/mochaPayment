265342b33d5a5a406c960004edef8624867cfb62
eric matutu <ericmatutu125@gmail.com>
2025-07-16 03:31:03 +0300

    jwt token to expire in 10min

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 // Generate JWT token
 const generateToken = (userId) => {
     return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-        expiresIn: '24h'
+        expiresIn: '10m'
     });
 };
 
