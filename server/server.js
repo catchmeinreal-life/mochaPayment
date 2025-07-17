4db0ec41ee0a4e0c7f58da5fc418327fa0ab8332
eric matutu <ericmatutu125@gmail.com>
2025-07-18 02:51:57 +0300

    switched to vercel

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
-        ? ['https://mochapayment-client.onrender.com'] 
+        ? [process.env.VERCEL_URL] 
         : ['http://localhost:5173', 'http://localhost:3000'],
 }));
 
