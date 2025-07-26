9b817131da1a55545d36380112e0e740d2ebd2d4
eric matutu <ericmatutu125@gmail.com>
2025-07-26 22:18:36 +0300

    run on local during dev and cloud in production .env

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,9 +5,11 @@ const connectDB = async () => {
     try {//mongodb://localhost:27017/
         if (process.env.NODE_ENV === 'production') {
             await mongoose.connect(process.env.MONGO_URI);
+            console.log('running on the cloud');
+        } else {
+            await mongoose.connect(process.env.MONGO_URI_local);
+            console.log('connected to mongoDb local');
         }
-        await mongoose.connect(process.env.MONGO_URI_local);
-        console.log('connected to mongoDb local');
         
     } catch (error) {
         console.error("problem connecting to database:", error.message);
