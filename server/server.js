f277c9c2a947a62e47953cbff248d15d21082a61
eric matutu <ericmatutu125@gmail.com>
2025-07-26 21:29:22 +0300

    new cors: http://localhost:5173

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
-        ? ['https://mochapayment-client.onrender.com'] 
+        ? ['https://mochapayment-client.onrender.com', 'http://localhost:5173' ] 
         : ['http://localhost:5173', 'http://localhost:3000'],
 }));
 
